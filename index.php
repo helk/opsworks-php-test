d8506127eb18986c890e2a65b03d7bb0bf31ff27
Andreas Helgegren <andreas.helgegren@gmail.com>
2019-01-23 15:05:38 +0100

    add user fetch query

--- index.php
+++ index.php
@@ -1,3 +1,66 @@
+<?php
+
+    function test() {
+        echo "hello";
+    }
+
+    function getOpsworksDBConfig() {
+        $db = NULL;
+        if (file_exists(dirname(__FILE__) . "/opsworks.php")){
+            require_once(dirname(__FILE__) . "/opsworks.php");
+            $opsWorks = new OpsWorks();
+            $db = $opsWorks->db;
+        }      
+        return $db;  
+    }
+
+    function connectDB() {
+        $pdo = NULL;
+        try {
+
+            $db = getOpsworksDBConfig();
+
+            if(!$db) {
+                die("no db config");
+            }
+
+            $dbHost = $db->host;
+            $dbName = $db->database;
+            $dbUser = $db->username;
+            $dbPassword = $db->password;
+
+            $pdo = new PDO("mysql:host=$dbHost;port=3306;dbname=$dbName",
+                $dbUser,
+                $dbPassword,
+                array( PDO::ATTR_PERSISTENT => false)
+            );
+
+        } catch (PDOException $e) {
+            print "Error!: " . $e->getMessage() . "<br/>";
+            die();
+        }
+
+        return $pdo;
+    }
+
+    function printUsers() {
+        $pdo = connectDB();
+        $stmt = $pdo->prepare("select * from user");
+        $stmt->execute();
+        while ($rs = $stmt->fetch(PDO::FETCH_OBJ)) {
+            echo "email: ".$rs->email."<br />";
+        }
+    }
+
+    // function addUser($email) {
+    //     $pdo = connectDB();
+    //     $sql = "INSERT INTO user (email) VALUES (?)";
+    //     $stmt= $pdo->prepare($sql);
+    //     $stmt->execute([$email]);
+    // }
+
+?>
+
 <html>
 <style>
     body {
@@ -8,69 +71,8 @@
 <h1>OpsWorks PHP App</h1>
 <div>
     <?php 
-        echo "hello";
+        printUsers();
     ?>
 </div>
 </body>
 </html>
-
-<?php
-
-    function test() {
-        echo "hello";
-    }
-
-    function opsworks() {
-        $hasOWDB = false;
-        echo "Hello2 ";
-        if (file_exists(dirname(__FILE__) . "/opsworks.php")){
-            echo "Hello3 ";
-            require_once(dirname(__FILE__) . "/opsworks.php");
-            $opsWorks = new OpsWorks();
-            $db = $opsWorks->db;
-            if ($db->host){
-                echo $db->host." ";
-                $hasOWDB = true;
-            }
-        }        
-    }
-
-    opsworks();
-
-    // function connectDB() {
-    //     try {
-
-    //         $host = getenv("DB_HOST");
-    //         $dbName = getenv("DB_NAME");
-    //         $dbUser = getenv("DB_USERNAME");
-    //         $dbPassword = getenv("DB_PASSWORD");
-
-    //         $dbh = new PDO("mysql:host=$dBName;port=3306;dbname=$dbName", $dbUser, $dbPassword,
-    //             array( PDO::ATTR_PERSISTENT => false)
-    //         );
-
-    //     } catch (PDOException $e) {
-    //         print "Error!: " . $e->getMessage() . "<br/>";
-    //         die();
-    //     }
-
-    //     return $dbh;
-    // }
-
-    // function printUsers() {
-    //     $pdo = connectDB();
-    //     $stmt = $pdo->prepare("select * from user");
-    //     $stmt->execute();
-    //     while ($rs = $stmt->fetch(PDO::FETCH_OBJ)) {
-    //         echo "email: ".$rs->email."<br />";
-    //     }
-    // }
-
-    // function addUser($email) {
-    //     $pdo = connectDB();
-    //     $sql = "INSERT INTO user (email) VALUES (?)";
-    //     $stmt= $pdo->prepare($sql);
-    //     $stmt->execute([$email]);
-    // }
-
-?>
