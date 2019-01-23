98c07754422c444bcf8cb2764ea6bc3575d93f6f
Andreas Helgegren <andreas.helgegren@gmail.com>
2019-01-23 14:54:13 +0100

    small changes

--- index.php
+++ index.php
@@ -16,40 +16,56 @@
 
 <?php
 
-    function connectDB() {
-        try {
-
-            $host = getenv("DB_HOST");
-            $dbName = getenv("DB_NAME");
-            $dbUser = getenv("DB_USERNAME");
-            $dbPassword = getenv("DB_PASSWORD");
-
-            $dbh = new PDO("mysql:host=$dBName;port=3306;dbname=$dbName", $dbUser, $dbPassword,
-                array( PDO::ATTR_PERSISTENT => false)
-            );
-
-        } catch (PDOException $e) {
-            print "Error!: " . $e->getMessage() . "<br/>";
-            die();
-        }
-
-        return $dbh;
+    function test() {
+        echo "hello";
     }
 
-    function printUsers() {
-        $pdo = connectDB();
-        $stmt = $pdo->prepare("select * from user");
-        $stmt->execute();
-        while ($rs = $stmt->fetch(PDO::FETCH_OBJ)) {
-            echo "email: ".$rs->email."<br />";
-        }
-    }
+    // function opsworks() {
+    //     $hasOWDB = false;
+    //     if (file_exists(dirname(__FILE__) . "/opsworks.php")){
+    //         require_once(dirname(__FILE__) . "/opsworks.php");
+    //         $opsWorks = new OpsWorks();
+    //         $db = $opsWorks->db;
+    //         if ($db->host){
+    //             $hasOWDB = true;
+    //         }
+    //     }        
+    // }
 
-    function addUser($email) {
-        $pdo = connectDB();
-        $sql = "INSERT INTO user (email) VALUES (?)";
-        $stmt= $pdo->prepare($sql);
-        $stmt->execute([$email]);
-    }
+    // function connectDB() {
+    //     try {
+
+    //         $host = getenv("DB_HOST");
+    //         $dbName = getenv("DB_NAME");
+    //         $dbUser = getenv("DB_USERNAME");
+    //         $dbPassword = getenv("DB_PASSWORD");
+
+    //         $dbh = new PDO("mysql:host=$dBName;port=3306;dbname=$dbName", $dbUser, $dbPassword,
+    //             array( PDO::ATTR_PERSISTENT => false)
+    //         );
+
+    //     } catch (PDOException $e) {
+    //         print "Error!: " . $e->getMessage() . "<br/>";
+    //         die();
+    //     }
+
+    //     return $dbh;
+    // }
+
+    // function printUsers() {
+    //     $pdo = connectDB();
+    //     $stmt = $pdo->prepare("select * from user");
+    //     $stmt->execute();
+    //     while ($rs = $stmt->fetch(PDO::FETCH_OBJ)) {
+    //         echo "email: ".$rs->email."<br />";
+    //     }
+    // }
+
+    // function addUser($email) {
+    //     $pdo = connectDB();
+    //     $sql = "INSERT INTO user (email) VALUES (?)";
+    //     $stmt= $pdo->prepare($sql);
+    //     $stmt->execute([$email]);
+    // }
 
 ?>
